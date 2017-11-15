3995a21e678a50ad3ae81e2ace48435ddd721c5d
Jonathan <jentin96@hotmail.com>
2017-11-15 13:34:06 +0100

    Library that has the 4 functions
    Created a library that has the 4 functions, will be used in the main file.

---
+++ Code/Test_file.h
@@ -0,0 +1,6 @@
+/* Library for the 4 calculation functions */
+
+   float Addition(float, float);
+   float Substract(float, float);
+   float Multiply(float, float);
+   float Divide(float, float);
